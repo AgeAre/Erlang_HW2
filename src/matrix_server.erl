c24c6351c58c423b1659603c133235e351855b3d
AgeAre <agearehr@gmail.com>
2019-05-20 21:12:49 +0300

    debug

--- src/matrix_server.erl
+++ src/matrix_server.erl
@@ -10,81 +10,87 @@
 -author("ageare").
 
 %% API
--export([start_server/0, shutdown/0, mult/2, get_version/0, explanation/0, restarter/0]).
+-export([start_server/0, shutdown/0, mult/2, get_version/0, explanation/0, restarter/0, loop/0]).
 
 %% TODO: Don't forget to flush(). messages. probably will be tested.
 
 
 start_server() ->
-  io:format("dad1"),
-  Pid = spawn(fun restarter/0).
+  spawn(fun() -> restarter() end).
 
 
 shutdown() ->
-  erlang:error(not_implemented).
+  case whereis(matrix_server) of
+    true -> matrix_server ! shutdown
+  end.
 
 mult(_Arg0, _Arg1) ->
+  % TODO: Call the mult function that will be used by messageing
   erlang:error(not_implemented).
 
-get_version() ->
-  erlang:error(not_implemented).
-
+get_version() -> version_1.
 explanation() -> {"to answer1"}.
 
 restarter()->
-  io:format("~ndad2"),
   process_flag(trap_exit, true),
-  %Pid = spawn_link(?MODULE, loop, []),
-  Pid = spawn_link(fun loop/0),
+  Pid = spawn_link(fun() -> ?MODULE:loop() end), %TODO try matrix_server:loop and see if it also works
   register(matrix_server, Pid),
   receive
-    %% Sanity check
-    {'EXIT', Pid, normal} -> io:format("normal~n"), ok;
-    {'EXIT', Pid, shutdown} -> io:format("shut~n"), ok;
-    {'EXIT', Pid, _} -> restarter()
-    %% End of sanity check
+    %{'EXIT', Pid, normal} -> ok;
+    {'EXIT', Pid, shutdown} -> ok;
+    {'EXIT', Pid, _} -> ?MODULE:restarter()
   end.
-%% TODO: loop should get State
+
 loop() ->
   receive
-  %% Sanity check
     {Pid, MsgRef, {multiple, Mat1, Mat2}} ->
       spawn(fun() -> mult(Pid, MsgRef, Mat1, Mat2) end),
-      loop();
+      ?MODULE:loop();
 
-    shutdown -> exit(whereis(matrix_server), shutdown);
+    shutdown -> exit(self(), shutdown);
 
     {Pid, MsgRef, get_version}->
       Pid ! {MsgRef, version_1},
       loop();
-    %% TODO: sw_ipgrade should be implemented
-    sw_upgrade -> io:format("Server's gotten a software upgrade request")
-  %% End of sanity check
+
+    sw_upgrade ->
+      ?MODULE:loop()
+
   end.
 
-for(0,_) ->
-  [];
-
-multVec({Row, Col}) ->
 
 
+
+dotProduct(A,B) when length(A) == length(B) -> dotProduct(A,B,0);
+dotProduct(_,_) -> erlang:error('Vectors must have the same length.').
+
+dotProduct([H1|T1],[H2|T2],P) -> dotProduct(T1,T2,P+H1*H2);
+dotProduct([],[],P) -> P.
+
+multVec(Pid, {I,J}, Mat1, Mat2) ->
+  Row = tuple_to_list(matrix:getRow(Mat1, I)),
+  Col = tuple_to_list(matrix:getCol(Mat2, J)),
+  Pid ! {dotProduct(Row, Col),I,J}.
 
 mult(Pid, MsgRef, Mat1, Mat2) ->
   Mat1_rows_num = tuple_size(Mat1),
   Mat2_cols_num = tuple_size(element(1,Mat2)),
-  ResMat = getZeroMat(Mat1_rows_num, Mat2_cols_num),
-  Rows = tuple_to_list(Mat1),
-  Cols = [matrix:getCol(Mat2,X) || X <- lists:seq(1,Mat2_cols_num)],
-  RowsXcols = [{X,Y} || X <- Rows, Y <- Cols],
-  [spawn(fun -> multVec(Tup) || Tup <- RowsXcols],
+  NumElements = Mat1_rows_num * Mat2_cols_num,
+%%  Rows = tuple_to_list(Mat1),
+%%  Cols = [matrix:getCol(Mat2,X) || X <- lists:seq(1,Mat2_cols_num)],
+%%  RxC = [{X,Y} || X <- Rows, Y <- Cols],
+  [spawn(fun() -> multVec(self(),{X,Y}, Mat1, Mat2) end) || X <- lists:seq(1, Mat1_rows_num), Y <- lists:seq(1,Mat2_cols_num)],
 
+  ZeroMat = matrix:getZeroMat(Mat1_rows_num, Mat2_cols_num),
   %% [{X,Y} || X <- lists:seq(1, Mat1_rows_num), Y <- lists:seq(1,Mat2_cols_num)].
-
-
-  receive
-    {Pid, MsgRef, {multiple, Mat1, Mat2}} ->
-
-  end.
+GetMsg = fun(_F,0, ResMat) -> Pid ! {MsgRef, ResMat};
+  (F,N,ResMat) when N > 0 ->
+    receive
+      {Res, I, J} ->
+        F(F,N - 1, matrix:setElementMat(I,J,ResMat,Res))
+    end
+ end,
+  GetMsg(GetMsg, NumElements, ZeroMat).
 
 %%mult(Mat1, Mat2) ->
 %%  receive
